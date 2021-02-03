0dbc7022b8d6cf7bc427d029623843a269a5772f
longhike <sam.ascheim@gmail.com>
2021-02-03 18:04:16 -0500

    updated static service for production env

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ const apiRouter = require("./routes/api-routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
-app.use(express.static("views"))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(apiRouter);
 
-app.use(apiRouter)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client", "build")));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
