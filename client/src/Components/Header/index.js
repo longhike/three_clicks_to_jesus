95ea99ebce94eec8f4423e4449d45de3d27ad55f
longhike <sam.ascheim@gmail.com>
2021-02-05 16:13:36 -0500

    cleaned up

--- client/src/Components/Header/index.js
+++ client/src/Components/Header/index.js
@@ -4,11 +4,10 @@ import { Navbar } from "react-bootstrap";
 function Header(props) {
   return (
     <Navbar bg="dark" variant="dark">
-      <Navbar.Brand onClick={props.resetGame} style={{ flex: 1 }}>
-        Three clicks to jesus
-      </Navbar.Brand>
+      <Navbar.Brand style={{ flex: 1 }}>Three clicks to jesus</Navbar.Brand>
+      <Navbar.Text onClick={props.resetGame}>Reset</Navbar.Text>
       <Navbar.Text onClick={() => props.setModalView(!props.modalView)}>
-        {props.modalView ? "X" : "Instructions"}
+        Instructions
       </Navbar.Text>
     </Navbar>
   );
